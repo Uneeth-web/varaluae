593315fa89fa7a5bb0c1d0d0b414e23861d5f9d8
Uneeth-web <74322645+Uneeth-web@users.noreply.github.com>
2023-01-14 16:06:10 +0530

    varaluae

---
+++ varal/js/main.js
@@ -0,0 +1,26 @@
+(function ($) {
+    "use strict";
+
+    
+    
+    
+    // Initiate the wowjs
+    new WOW().init();
+
+
+    // Sticky Navbar
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 45) {
+            $('.navbar').addClass('sticky-top shadow-sm');
+        } else {
+            $('.navbar').removeClass('sticky-top shadow-sm');
+        }
+    });
+    
+    
+    
+    
+})(jQuery);
+
+
+
